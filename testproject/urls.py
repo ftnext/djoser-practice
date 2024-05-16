b0199877beb7ea9ba8835ad69b51d2a20facf270
ftnext <takuyafjp+develop@gmail.com>
2024-05-16 23:13:23 +0900

    [feat] Token Authorization

--- testproject/urls.py
+++ testproject/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
+    path("auth/", include("djoser.urls.base")),
+    path("auth/", include("djoser.urls.authtoken")),
     path("admin/", admin.site.urls),
 ]
