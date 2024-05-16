f76e3dedb9d29c05ac48fd505e7f3137878c90bd
ftnext <takuyafjp+develop@gmail.com>
2024-05-16 23:24:24 +0900

    [feat] Support Bearer token instead of Token

--- testproject/settings.py
+++ testproject/settings.py
@@ -129,6 +129,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework.authentication.TokenAuthentication",
+        "authentication.BearerTokenAuthentication",
     ],
 }
